ec80cc9f33d583cd7865ba9ed760246db55607d8
Mika.dev <charmikadev@gmail.com>
2022-10-03 23:50:41 +0100

    pages/components layout

---
+++ components/Footer.js
@@ -0,0 +1,31 @@
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className='footer-container'>
+                <ul className='language-list'>
+                    <li id='selected-lang'>English (UK)</li>
+                    <li>Polski</li>
+                    <li>Español</li>
+                    <li>Français</li>
+                    <li>Italiano</li>
+                    <li>Lietuvių</li>
+                    <li>Română</li>
+                    <li>中文</li>
+                    <li>Português</li>
+                    <li>Deutsch</li>
+                </ul>
+                <ul className='pageFooterLinkList'>
+                    <a href='/'><li>Log In</li></a>
+                    <a href='/sign-up'><li>Sign Up</li></a>
+                    <a href='https://github.com/Mikadev1996'><li>Mika's GitHub</li></a>
+                    <a href='/home'><li>Home</li></a>
+                </ul>
+                <ul>
+                    <li>Mika © 2022</li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
---
+++ pages/signup.js
@@ -0,0 +1,50 @@
+
+export default function SignUp() {
+    const url = config.url.BASE_URL;
+    return (
+        <div className='app'>
+            <div className="signin-content account-content">
+                <div id='sign-up-page-header' className='title-container'><h1>Mika's facebook</h1></div>
+                <div id="form-container">
+                    <div className="form-header">
+                        <h2>Create a new account</h2>
+                        <p>It's quick and easy</p>
+                    </div>
+                    <form className='account-form'  action={`${url}/user/sign-up`}method='POST'>
+                        <div className="form-control name-info">
+                            <input type="text" placeholder="First name" id="firstname" name='firstname'/>
+                            <input type="text" placeholder="Surname" id="surname" name='surname'/>
+                        </div>
+
+                        <div className="form-control">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" placeholder="Username" id="username" name='username'/>
+                            <small>Error Message</small>
+                        </div>
+
+                        <div className="form-control">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" placeholder="New password" id="password" maxLength="16" name='password'/>
+                            <small>Error Message</small>
+                        </div>
+
+                        <div className='form-control'>
+                            <label htmlFor='date_of_birth'>Date of Birth</label>
+                            <input type='date' id='date_of_birth' name='date_of_birth'/>
+                            <small>Error Message</small>
+                        </div>
+
+                        <div>
+                            <button className='form-submit' type="submit">Sign Up</button>
+                        </div>
+                        <div className='already-have'>
+                            <a href='/'><p>Already have an account?</p></a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <Footer />
+        </div>
+    )
+}
