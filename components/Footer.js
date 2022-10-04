661e2cb610cdd9c7fe3a881add394d65f34b214c
Mika.dev <charmikadev@gmail.com>
2022-10-04 21:18:52 +0100

    footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,12 @@
-
+import styles from '../styles/Footer.module.css';
+import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer>
-            <div className='footer-container'>
-                <ul className='language-list'>
-                    <li id='selected-lang'>English (UK)</li>
+        <footer className={styles.footer}>
+            <div>
+                <ul className={styles.footer_list}>
+                    <li className={styles.selected_lang}>English (UK)</li>
                     <li>Polski</li>
                     <li>Español</li>
                     <li>Français</li>
@@ -16,13 +17,13 @@ export default function Footer() {
                     <li>Português</li>
                     <li>Deutsch</li>
                 </ul>
-                <ul className='pageFooterLinkList'>
-                    <a href='/'><li>Log In</li></a>
-                    <a href='/sign-up'><li>Sign Up</li></a>
-                    <a href='https://github.com/Mikadev1996'><li>Mika's GitHub</li></a>
-                    <a href='/home'><li>Home</li></a>
+                <ul className={styles.footer_info}>
+                    <Link href='/'><li>Log In</li></Link>
+                    <Link href='/sign-up'><li>Sign Up</li></Link>
+                    <Link href='https://github.com/Mikadev1996'><li>Mika's GitHub</li></Link>
+                    <Link href='/home'><li>Home</li></Link>
                 </ul>
-                <ul>
+                <ul className={styles.footer_info}>
                     <li>Mika © 2022</li>
                 </ul>
             </div>
